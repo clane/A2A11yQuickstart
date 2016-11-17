deca15b27b8b058603ea9073d0db8727392288fc
Chris Lane <chris.lane359@gmail.com>
2016-11-17 06:49:34 -0800

    selenium jar location
    Just discovered there is no need to specify the selenium jar location
    in the protractor config file

--- angus/protractor.config.js
+++ angus/protractor.config.js
@@ -20,11 +20,10 @@ exports.config = {
 
 
 //OSX
-//chromeDriver: '/usr/local/lib/node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.25',
-//seleniumServerJar: '/usr/local/lib/node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar',
+chromeDriver: '/usr/local/lib/node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.25',
 
 //Windows
-chromeDriver: '.\\node_modules\\chromedriver\\lib\\chromedriver\\chromedriver.exe',
+//chromeDriver: '.\\node_modules\\chromedriver\\lib\\chromedriver\\chromedriver.exe',
 
 
 
