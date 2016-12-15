8b9c213fd02f1817f8b5f37a8ab0800a166a2589
Chris Lane <chris.lane359@gmail.com>
2016-12-14 19:40:46 -0800

    now binding to down arrow events

--- repo/app/app.component.ts
+++ repo/app/app.component.ts
@@ -167,22 +167,191 @@ export class ModalDialog  {
 @Component({
   selector: "combo-box",
   template: `
+
+  <style>
+
+  * {
+  font-family: "Times New Roman,Georgia,Serif"; 
+}
+.hidden {
+  position: absolute;
+  top: -20em;
+  left: -200em;
+}
+
+.wrapper {
+  height: 24px;
+  overflow: auto;
+}
+.cb {
+  margin: 20px;
+  padding: 0;
+  height: 24px;
+  display: block;
+  overflow: visible;
+}
+
+.cb_label {
+  margin: 0;
+  padding: 2px;
+  width: 45px;
+  font-weight: bold;
+  float: left;
+  display: inline;
+}
+.cb_edit {
+  margin: 0;
+  padding: 2px 3px;
+  width: 240px;
+  height: 18px;
+  border: 1px solid black;
+  font-size: 1em;
+  float: left;
+  display: inline;
+}
+.cb_button {
+  margin: 0;
+  padding: 0;
+  height: 24px;
+  width: 24px;
+  border: 1px solid black;
+  background-color: #999;
+  float: left;
+  display: inline;
+  text-align: center;
+}
+
+button.cb_button img {
+  margin: 0;
+  padding: 0;
+  height: 22px;
+  width: 22px;
+  position: relative;
+  top: -1px;
+  left: -3px;
+}
+.cb_list {
+  clear: both;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  margin-left: 49px;
+  border: 1px solid black;
+  width: 246px;
+  height: 200px;
+  overflow: auto;
+  background-color: #fff;
+  position: relative;
+  z-index: 300;
+}
+
+.cb_option {
+  margin: 0 1px 0 0;
+  padding: 2px 5px;
+}
+.selected {
+  border-top: 1px solid #44e;
+  border-bottom: 1px solid #44e;
+  padding: 1px 5px;
+  background-color: #77a;
+  color: #fff;
+}
+.cb_option:hover {
+  border-top: 1px solid #44e;
+  border-bottom: 1px solid #44e;
+  padding: 1px 5px;
+  font-weight: bold;
+  background-color: #77f;
+  color: #fff;
+}
+</style>
+
     <h2>ComboBox</h2>
-    <label for="comboBoxInput">ComboBox Input</label>
-    <input id="comboBoxInput" type="text" (keyup)="moveFocus()">
-    <div tabindex="-1" #focusMe>alsjkdfksdfj</div>
+
+    <div id="cb1" class="cb" role="combobox">
+      <div class="cb_label"><label id="cb1-label" for="cb1-edit">State</label>:</div>
+      <input (keydown.ArrowDown)="handleKeyEvent()" id="cb1-edit" class="cb_edit" type="text" 
+        aria-labelledby="cb1-label"
+        aria-autocomplete="inline"
+        aria-owns="cb1-list"/>
+      <div id="cb1-button-label" class="hidden">Open list of states</div>
+      <button  (click)="toggleExpanded()" id="cb1-button" class="cb_button" aria-labelledby="cb1-button-label" aria-controls="cb1-list" tabindex="-1">
+        <img src="http://www.oaa-accessibility.org/media/examples/images/button-arrow-down.png" alt="Open or close the list box" />
+      </button>
+
+      <ul *ngIf="expanded" id="cb1-list" class="cb_list" tabindex="-1" role="listbox" [attr.aria-expanded]="expanded">
+        <li id="cb1-opt1" role="option" class="cb_option">Alabama</li>
+        <li id="cb1-opt2" role="option" class="cb_option">Alaska</li>
+        <li id="cb1-opt3" role="option" class="cb_option">American Samoa</li>
+        <li id="cb1-opt4" role="option" class="cb_option">Arizona</li>
+        <li id="cb1-opt5" role="option" class="cb_option">Arkansas</li>
+        <li id="cb1-opt6" role="option" class="cb_option">California</li>
+        <li id="cb1-opt7" role="option" class="cb_option">Colorado</li>
+        <li id="cb1-opt8" role="option" class="cb_option">Connecticut</li>
+        <li id="cb1-opt9" role="option" class="cb_option">Delaware</li>
+        <li id="cb1-opt10" role="option" class="cb_option">District of Columbia</li>
+        <li id="cb1-opt11" role="option" class="cb_option">Florida</li>
+        <li id="cb1-opt12" role="option" class="cb_option">Georgia</li>
+        <li id="cb1-opt13" role="option" class="cb_option">Guam</li>
+        <li id="cb1-opt14" role="option" class="cb_option">Hawaii</li>
+        <li id="cb1-opt15" role="option" class="cb_option">Idaho</li>
+        <li id="cb1-opt16" role="option" class="cb_option selected">Illinois</li>
+        <li id="cb1-opt17" role="option" class="cb_option">Indiana</li>
+        <li id="cb1-opt18" role="option" class="cb_option">Iowa</li>
+        <li id="cb1-opt19" role="option" class="cb_option">Kansas</li>
+        <li id="cb1-opt20" role="option" class="cb_option">Kentucky</li>
+        <li id="cb1-opt21" role="option" class="cb_option">Louisiana</li>
+        <li id="cb1-opt22" role="option" class="cb_option">Maine</li>
+        <li id="cb1-opt23" role="option" class="cb_option">Maryland</li>
+        <li id="cb1-opt24" role="option" class="cb_option">Massachusetts</li>
+        <li id="cb1-opt25" role="option" class="cb_option">Michigan</li>
+        <li id="cb1-opt26" role="option" class="cb_option">Minnesota</li>
+        <li id="cb1-opt27" role="option" class="cb_option">Mississippi</li>
+        <li id="cb1-opt28" role="option" class="cb_option">Missouri</li>
+        <li id="cb1-opt29" role="option" class="cb_option">Montana</li>
+        <li id="cb1-opt30" role="option" class="cb_option">Nebraska</li>
+        <li id="cb1-opt31" role="option" class="cb_option">Nevada</li>
+        <li id="cb1-opt32" role="option" class="cb_option">New Hampshire</li>
+        <li id="cb1-opt33" role="option" class="cb_option">New Jersey</li>
+        <li id="cb1-opt34" role="option" class="cb_option">New Mexico</li>
+        <li id="cb1-opt35" role="option" class="cb_option">New York</li>
+        <li id="cb1-opt36" role="option" class="cb_option">North Carolina</li>
+        <li id="cb1-opt37" role="option" class="cb_option">North Dakota</li>
+        <li id="cb1-opt38" role="option" class="cb_option">Northern Marianas Islands</li>
+        <li id="cb1-opt39" role="option" class="cb_option">Ohio</li>
+        <li id="cb1-opt40" role="option" class="cb_option">Oklahoma</li>
+        <li id="cb1-opt41" role="option" class="cb_option">Oregon</li>
+        <li id="cb1-opt42" role="option" class="cb_option">Pennsylvania</li>
+        <li id="cb1-opt43" role="option" class="cb_option">Puerto Rico</li>
+        <li id="cb1-opt44" role="option" class="cb_option">Rhode Island</li>
+        <li id="cb1-opt45" role="option" class="cb_option">South Carolina</li>
+        <li id="cb1-opt47" role="option" class="cb_option">South Dakota</li>
+        <li id="cb1-opt48" role="option" class="cb_option">Tennessee</li>
+        <li id="cb1-opt49" role="option" class="cb_option">Texas</li>
+        <li id="cb1-opt50" role="option" class="cb_option">Utah</li>
+        <li id="cb1-opt51" role="option" class="cb_option">Vermont</li>
+        <li id="cb1-opt52" role="option" class="cb_option">Virginia</li>
+        <li id="cb1-opt53" role="option" class="cb_option">Virgin Islands</li>
+        <li id="cb1-opt54" role="option" class="cb_option">Washington</li>
+        <li id="cb1-opt55" role="option" class="cb_option">West Virginia</li>
+        <li id="cb1-opt56" role="option" class="cb_option">Wisconsin</li>
+        <li id="cb1-opt57" role="option" class="cb_option">Wyoming</li>
+      </ul>
+    </div>
+
   ` 
 })
 
 export class ComboBox  {
 
-  constructor(private renderer: Renderer) {}
-  @ViewChild('focusMe') focusTarget: ElementRef;
+  expanded: boolean = false; 
 
-  moveFocus(){
-      this.renderer.invokeElementMethod(this.focusTarget.nativeElement, 'focus');
+  toggleExpanded() {
+    this.expanded = !this.expanded
   }
 
+  handleKeyEvent() { alert('key event happened'); }
+  
 }
 
 @Component({
