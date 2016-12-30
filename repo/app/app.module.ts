4f1e97c3184b1acafc2d8a0689f1ba3e70650aef
Chris Lane <chris.lane359@gmail.com>
2016-12-30 12:24:22 -0800

    working on routing

--- repo/app/app.module.ts
+++ repo/app/app.module.ts
@@ -1,10 +1,26 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
-import { TooltipComponent, AccordionComponent, AlertComponent, ModalOpenButton, ModalDialog, ComboBox, ListBoxComponent, WidgetDemoComponent }  from './app.component';
+import { 
+  TooltipComponent, 
+  AccordionComponent,
+  AlertComponent,
+  ModalOpenButton,
+  ModalDialog, 
+  ComboBox, 
+  ListBoxComponent, 
+  WidgetDemoComponent }  from './app.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ TooltipComponent, AccordionComponent, AlertComponent, ModalOpenButton, ModalDialog, ComboBox, ListBoxComponent, WidgetDemoComponent],
+  declarations: [ TooltipComponent,
+                  AccordionComponent, 
+                  AlertComponent, 
+                  ModalOpenButton, 
+                  ModalDialog, 
+                  ComboBox, 
+                  ListBoxComponent, 
+                  WidgetDemoComponent],
+
   bootstrap: [ WidgetDemoComponent]
 })
 export class AppModule { }
