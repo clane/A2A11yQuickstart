417a861b95affd89453917a442367d9f49957571
Chris Lane <chris.lane359@gmail.com>
2017-01-02 23:34:27 -0800

    saving

--- repo/app/modal.service.ts
+++ repo/app/modal.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Subject }    from 'rxjs/Subject';
+
 
 @Injectable()
 export class ModalService {
+
+  // Observable sources
+  private isModalSource = new Subject<boolean>();
+ 
+  // Observable streams
+  isModalChanged$ = this.isModalSource.asObservable();
+
+
   isModal:boolean = false;
 }
