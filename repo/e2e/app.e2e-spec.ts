d5a67e7dd1dd68c2cb58b3adf6701e0926993245
Chris Lane <chris.lane359@gmail.com>
2017-01-06 23:31:29 -0800

    now have title tag test

--- repo/e2e/app.e2e-spec.ts
+++ repo/e2e/app.e2e-spec.ts
@@ -1,16 +1,25 @@
 
 describe('A11y QuickStart E2E Tests', function () {
 
+    //needed to keep the Jasmine spec from timing out
     browser.ignoreSynchronization = true;
-
-    let lang = 'en';
   
     beforeEach(function () {
         browser.get('http://localhost:8080');
     });
 
-    it('html tag lang attribute should be: ' + lang, function () {
+    //Verity the lang attribute
+    let lang = 'en';
+    it('The value of the HTML tags lang attribute should be: ' + lang, function () {
         expect(element(by.css('html')).getAttribute('lang')).toEqual(lang);
     });
+
+    let expectedTitle = 'A2i A11y Quickstart';
+    it('The title tag should be: ' + expectedTitle, function () {
+    	expect(browser.getTitle()).toEqual(expectedTitle);
+    });
+
+
+
 
 });
