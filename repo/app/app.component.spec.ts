12fba418710052f829d62acd08a9fc7f4f13ae93
Chris Lane <chris.lane359@gmail.com>
2017-01-07 21:43:15 -0800

    3 unit tests working

--- repo/app/app.component.spec.ts
+++ repo/app/app.component.spec.ts
@@ -1,21 +1,32 @@
 /* tslint:disable:no-unused-variable */
 import { TooltipComponent } from './tooltip.component';
+import { AccordionComponent } from './accordion.component';
+import { AlertComponent } from './alert.component';
 
 import { TestBed }      from '@angular/core/testing';
-
 import { By }           from '@angular/platform-browser';
 
 ////////  SPECS  /////////////
 
 
-describe('TooltipComponent with TCB', function () {
+describe('Testing widget demo', function () {
   beforeEach(() => {
-    TestBed.configureTestingModule({declarations: [TooltipComponent]});
+    TestBed.configureTestingModule({declarations: [TooltipComponent, AccordionComponent, AlertComponent]});
   });
 
- it('should instantiate component', () => {
+  it('should instantiate a tooltip component', () => {
     let fixture = TestBed.createComponent(TooltipComponent);
     expect(fixture.componentInstance instanceof TooltipComponent).toBe(true, 'should create TooltipComponent');
+  });
+
+  it('should instantiate an accordion component', () => {
+    let fixture = TestBed.createComponent(AccordionComponent);
+    expect(fixture.componentInstance instanceof AccordionComponent).toBe(true, 'should create AccordionComponent');
+  });
+
+  it('should instantiate an alert component', () => {
+    let fixture = TestBed.createComponent(AlertComponent);
+    expect(fixture.componentInstance instanceof AlertComponent).toBe(true, 'should create AlertComponent');
   });
 
 });
