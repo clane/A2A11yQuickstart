1564833c32c3b5fbb969ec20985ab4e27773ed0f
Chris Lane <chris.lane359@gmail.com>
2017-01-07 21:23:05 -0800

    got one Karma test working
    won’t be able to test AppComponent due to routing issue

--- repo/app/app.component.spec.ts
+++ repo/app/app.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { AppComponent } from './app.component';
+import { TooltipComponent } from './tooltip.component';
 
 import { TestBed }      from '@angular/core/testing';
 
@@ -7,32 +7,15 @@ import { By }           from '@angular/platform-browser';
 
 ////////  SPECS  /////////////
 
-/// Delete this
-describe('Smoke test', () => {
-  it('should run a passing test', () => {
-    expect(true).toEqual(true, 'should pass');
-  });
-});
 
-describe('AppComponent with TCB', function () {
+describe('TooltipComponent with TCB', function () {
   beforeEach(() => {
-    TestBed.configureTestingModule({declarations: [AppComponent]});
+    TestBed.configureTestingModule({declarations: [TooltipComponent]});
   });
 
-  it('should instantiate component', () => {
-    let fixture = TestBed.createComponent(AppComponent);
-    expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
+ it('should instantiate component', () => {
+    let fixture = TestBed.createComponent(TooltipComponent);
+    expect(fixture.componentInstance instanceof TooltipComponent).toBe(true, 'should create TooltipComponent');
   });
 
-  it('should have expected <h2> text', () => {
-    let fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-
-    let h2 = fixture.debugElement.query(el => el.name === 'h2').nativeElement;  // it works
-
-        h2 = fixture.debugElement.query(By.css('h2')).nativeElement;            // preferred
-
-        expect(h2.innerText).toMatch(/Alert/i),
-        '<h2> should display the widget');
-  });
 });
