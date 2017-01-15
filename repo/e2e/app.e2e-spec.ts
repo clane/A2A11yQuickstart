dafcf85852ce55a044116df88f15e188002eb858
Chris Lane <chris.lane359@gmail.com>
2017-01-14 16:23:44 -0800

    looks like a bare minimum end to end test, lang attribute only

--- repo/e2e/app.e2e-spec.ts
+++ repo/e2e/app.e2e-spec.ts
@@ -1,15 +1,7 @@
 
 describe('A11y QuickStart E2E Tests', function () {
-
-    //needed to keep the Jasmine spec from timing out
-    browser.ignoreSynchronization = true;
-  
-
-    //Verity the lang attribute
     let lang = 'en';
     it('The value of the HTML tags lang attribute should be: ' + lang, function () {
         expect(element(by.css('html')).getAttribute('lang')).toEqual(lang);
     });
-
-
 });
