fcd916b220d05a3478b6fb37772ab9503117c803
Chris Lane <chris.lane359@gmail.com>
2017-01-15 20:30:21 -0800

    lang test working, was broken again

--- repo/e2e/app.e2e-spec.ts
+++ repo/e2e/app.e2e-spec.ts
@@ -1,3 +1,17 @@
 
 describe('A11y QuickStart E2E Tests', function () {
+
+    var lang = 'en';
+
+    beforeEach(function () {
+        browser.get('http://localhost:8080');
+    });
+
+    it('html tag lang attribute should be: ' + lang, function () {
+        expect(element(by.css('html')).getAttribute('lang')).toEqual(lang);
+    });
+
+
+
+
 });
