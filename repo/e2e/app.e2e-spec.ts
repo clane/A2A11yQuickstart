09c93c487d8614875f6dcd2ed8fb1cecf71272ef
Chris Lane <chris.lane359@gmail.com>
2017-01-17 06:21:08 -0800

    fixing routing title tests, could be a timing issue with first router link

--- repo/e2e/app.e2e-spec.ts
+++ repo/e2e/app.e2e-spec.ts
@@ -15,55 +15,27 @@ describe('A11y QuickStart E2E Tests', function () {
     //Begin title specs
     let expectedTitle:string;
     let title:any;
+
+
     expectedTitle = 'Tooltip';
     it('After activating the ' + expectedTitle + ' link the title should be ' + expectedTitle, function () {
         let navLink:any = element(by.css('[routerlink="/tooltip"]'));
         navLink.click();
-        let expectedTitle  = browser.getTitle().then(function(title){});
-        expect(expectedTitle).toEqual(title);
+        browser.getTitle().then(function(title){
+            expect(expectedTitle).toEqual(title);
+        });
     });
 
     expectedTitle = 'Accordion';
     it('After activating the ' + expectedTitle + ' link the title should be ' + expectedTitle, function () {
-        let navLink:any = element(by.css('[routerlink="/tooltip"]'));
+        let navLink:any = element(by.css('[routerlink="/accordion"]'));
         navLink.click();
-        let expectedTitle  = browser.getTitle().then(function(title){});
-        expect(expectedTitle).toEqual(title);
-    });
-
-    expectedTitle = 'Alerts';
-    it('After activating the ' + expectedTitle + ' link the title should be ' + expectedTitle, function () {
-        let navLink:any = element(by.css('[routerlink="/tooltip"]'));
-        navLink.click();
-        let expectedTitle  = browser.getTitle().then(function(title){});
-        expect(expectedTitle).toEqual(title);
-    });
-
-    expectedTitle = 'Modal';
-    it('After activating the ' + expectedTitle + ' link the title should be ' + expectedTitle, function () {
-        let navLink:any = element(by.css('[routerlink="/tooltip"]'));
-        navLink.click();
-        let expectedTitle  = browser.getTitle().then(function(title){});
-        expect(expectedTitle).toEqual(title);
-    });
-
-    expectedTitle = 'Combobox`';
-    it('After activating the ' + expectedTitle + ' link the title should be ' + expectedTitle, function () {
-        let navLink:any = element(by.css('[routerlink="/tooltip"]'));
-        navLink.click();
-        let expectedTitle  = browser.getTitle().then(function(title){});
-        expect(expectedTitle).toEqual(title);
+        browser.getTitle().then(function(title){
+            expect(expectedTitle).toEqual(title);
+        });
     });
 
     //End
 
-    //Begin test for <body> as active element
-    it('Test for the tag name of the active element to be "BODY" after each browser.get call', function () {
-         browser.executeScript('return document.activeElement.tagName').then(function (tagName){
-             expect(tagName).toEqual('BODY');
-         });
-    });
-    //End test for <body> as active element
-
-
+   
 });
