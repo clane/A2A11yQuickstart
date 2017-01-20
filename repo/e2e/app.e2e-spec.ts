8278ccbb4bc6dfe08153e2ec3c267e9eafc76fdd
Chris Lane <chris.lane359@gmail.com>
2017-01-20 09:45:19 -0800

    all tests pass at 41 milliseconds

--- repo/e2e/app.e2e-spec.ts
+++ repo/e2e/app.e2e-spec.ts
@@ -16,7 +16,7 @@ describe('A11y QuickStart E2E Tests',  () => {
             //queue 100ms wait
             origFn.call(browser.driver.controlFlow(), function() {
                 //return protractor.promise.delayed(100);
-                return protractor.promise.delayed(30);//all tests pass at 30 milliseconds
+                return protractor.promise.delayed(41);//all tests pass at 41 milliseconds
             });
                 return origFn.apply(browser.driver.controlFlow(), args);
             };
