11ca35e9658bd2daea718fc2c88edb611dafb94f
Chris Lane <chris.lane359@gmail.com>
2017-01-20 23:21:49 -0800

    Revert "cleanup and adding arrow functions"
    This reverts commit e35e9b44793f5d1a62a1601b2594dcef52791267.

--- repo/e2e/app.e2e-spec.ts
+++ repo/e2e/app.e2e-spec.ts
@@ -1,19 +1,26 @@
 
 describe('A11y QuickStart E2E Tests',  () => {
     
+   
     browser.get('http://localhost:8080');
 
     beforeEach(function () {
+        //Delay before each spec for presentation purposes
+        //browser.sleep(5000);
       
         //Slows things down but breaks some tests
-        var origFn = browser.driver.controlFlow().execute;
-        browser.driver.controlFlow().execute = () => {
-        var args = arguments;
-        origFn.call(browser.driver.controlFlow(), () => {
-            return protractor.promise.delayed(41);//all tests pass at 41 milliseconds
-        });
-            return origFn.apply(browser.driver.controlFlow(), args);
-        };
+       
+            var origFn = browser.driver.controlFlow().execute;
+            browser.driver.controlFlow().execute = function() {
+            var args = arguments;
+            //queue 100ms wait
+            origFn.call(browser.driver.controlFlow(), function() {
+                //return protractor.promise.delayed(100);
+                return protractor.promise.delayed(41);//all tests pass at 41 milliseconds
+            });
+                return origFn.apply(browser.driver.controlFlow(), args);
+            };
+        
 
     });
 
@@ -51,6 +58,8 @@ describe('A11y QuickStart E2E Tests',  () => {
 
         });
     });
+
+   
 
 
 });
