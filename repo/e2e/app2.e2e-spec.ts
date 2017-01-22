1345a199f4b349606f4f22531410dab0e3ce1348
Chris Lane <chris.lane359@gmail.com>
2017-01-21 16:08:05 -0800

    almost all route and title tests now passing yay!
     angular-quickstart@1.0.0 e2e /Users/developer/Desktop/eddie/repo
    > concurrently "npm run tsc:w" "http-server -s" "protractor
    protractor.config.js" --kill-others --success first
    
    [0]
    [0] > angular-quickstart@1.0.0 tsc:w /Users/developer/Desktop/eddie/repo
    [0] > tsc -w
    [0]
    [2] [16:05:31] I/direct - Using ChromeDriver directly...
    [2] [16:05:31] I/launcher - Running 1 instances of WebDriver
    [2] AppDir: ./
    [2]   Suite: Suite 1
    [0] 4:05:36 PM - Compilation complete. Watching for file changes.
    [2]     passed - Test for the tag name of the active element to be
    "BODY" after the page loads
    [2]     passed - html tag lang attribute should be: en
    [2]     passed - Test of default route
    [2]     passed - Test of accordion route
    [2]   Suite passed: Suite 1
    [2]   Suite: Suite 2
    [2]     passed - Test of tooltip route
    [2]     passed - Test of alert route
    [2]     passed - Test of modal route
    [2]     passed - Test of tooltip route
    [2]     passed - Test of alert route
    [2]     passed - Test of modal route
    [2]     passed - Test of tooltip route
    [2]     passed - Test of alert route
    [2]     passed - Test of modal route
    [2]     passed - Test of accordion route
    [2]     passed - Test of accordion route
    [2]     passed - Test of accordion route and title
    [2]     passed - Test of tooltip route and title
    [2]     passed - Test of modal route and title
    [2]     passed - Test of alert route and title
    [2]     failed - Test of combobox route and title
    [2]   Suite failed: Suite 2

--- repo/e2e/app2.e2e-spec.ts
+++ repo/e2e/app2.e2e-spec.ts
@@ -52,6 +52,82 @@ describe('Suite 2',  () => {
         navLink.click();
     });
 
+    let expectedTitle:any;
+
+    expectedTitle = 'Tooltip';
+    
+    it('Test of accordion route', () => {
+        expectedTitle = 'Accordion';
+        navLink = element(by.css('[routerlink="/accordion"]'));
+        navLink.click();
+        browser.getTitle().then((title) => {
+            expect(expectedTitle).toEqual(title);
+        });
+    });
+
+     it('Test of accordion route', () => {
+        expectedTitle = 'Accordion';
+        navLink = element(by.css('[routerlink="/accordion"]'));
+        navLink.click();
+        browser.getTitle().then((title) => {
+            expect(expectedTitle).toEqual(title);
+        });
+    });
+
+
+    it('Test of accordion route and title', () => {
+        expectedTitle = 'Accordion';
+        navLink = element(by.css('[routerlink="/accordion"]'));
+        navLink.click();
+        browser.getTitle().then((title) => {
+            expect(expectedTitle).toEqual(title);
+        });
+    });
+
+     it('Test of tooltip route and title', () => {
+        expectedTitle = 'Tooltip';
+        navLink = element(by.css('[routerlink="/tooltip"]'));
+        navLink.click();
+        browser.getTitle().then((title) => {
+            expect(expectedTitle).toEqual(title);
+        });
+    });
+
+
+     it('Test of modal route and title', () => {
+        expectedTitle = 'Modal';
+        navLink = element(by.css('[routerlink="/modal"]'));
+        navLink.click();
+        browser.getTitle().then((title) => {
+            expect(expectedTitle).toEqual(title);
+        });
+    });
+
+    it('Test of alert route and title', () => {
+        expectedTitle = 'Alerts';
+        navLink = element(by.css('[routerlink="/alert"]'));
+        navLink.click();
+        browser.getTitle().then((title) => {
+            expect(expectedTitle).toEqual(title);
+        });
+    });
+
+    it('Test of combobox route and title', () => {
+        expectedTitle = 'combobox';
+        navLink = element(by.css('[routerlink="/combobox"]'));
+        navLink.click();
+        browser.getTitle().then((title) => {
+            expect(expectedTitle).toEqual(title);
+        });
+    });
+
+
+
+
+
+
+
+
 
 
 
