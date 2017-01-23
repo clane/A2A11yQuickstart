638bc569bd01af9fa2d2665755e5ea0955278a21
Chris Lane <chris.lane359@gmail.com>
2017-01-22 20:50:48 -0800

    cleanup

--- repo/app/listbox.component.ts
+++ repo/app/listbox.component.ts
@@ -36,87 +36,87 @@ import { StateService } from './state.service';
 })
 export class ListBoxComponent {
 
-  constructor(private stateService: StateService, private renderer: Renderer) {}
-  @Output() onListboxOptionSelected = new EventEmitter<string>();
-  @Output() onListboxEscPressed = new EventEmitter<boolean>();
-  @ViewChildren('option') options: any;
-  @Input('selectedId') focusIndex: number;
-  states: State[];
-  @Input() state: State;
+    constructor(private stateService: StateService, private renderer: Renderer) {}
+    @Output() onListboxOptionSelected = new EventEmitter<string>();
+    @Output() onListboxEscPressed = new EventEmitter<boolean>();
+    @ViewChildren('option') options: any;
+    @Input('selectedId') focusIndex: number;
+    states: State[];
+    @Input() state: State;
 
-  getStates(): void {
-      this.stateService.getStates().then(states => this.states = states);
-  }
+    getStates(): void {
+        this.stateService.getStates().then(states => this.states = states);
+    }
 
-  focusOption(optionIndex: number) {
-      setTimeout( ()=>{  
-          this.renderer.invokeElementMethod(this.options._results[optionIndex].nativeElement, 'focus');
-      }, 0);
-  }
+    focusOption(optionIndex: number) {
+        setTimeout( ()=>{  
+            this.renderer.invokeElementMethod(this.options._results[optionIndex].nativeElement, 'focus');
+        }, 0);
+    }
 
-  focusNextOption() {
-      setTimeout( ()=>{  
-          let optionsLength: number = this.options._results.length;
-          if ( this.focusIndex === (optionsLength - 1 )){
+    focusNextOption() {
+        setTimeout( ()=>{  
+            let optionsLength: number = this.options._results.length;
+            if (this.focusIndex === (optionsLength - 1 )){
               this.focusIndex = 0;
-          } else {
-              this.focusIndex = this.focusIndex + 1;
-          }
-          this.focusOption(this.focusIndex);
-      }, 0);
-  }
+            } else {
+                this.focusIndex = this.focusIndex + 1;
+            }
+            this.focusOption(this.focusIndex);
+        }, 0);
+    }
 
-  focusPrevOption() {
-      setTimeout( ()=>{ 
-          let optionsLength: number = this.options._results.length;
-          if( this.focusIndex === 0){
-              this.focusIndex = optionsLength - 1;
-          } else {
-              this.focusIndex = this.focusIndex - 1;
-          }
-          this.focusOption(this.focusIndex);
-      }, 0);
-  }
+    focusPrevOption() {
+        setTimeout( ()=>{ 
+            let optionsLength: number = this.options._results.length;
+            if( this.focusIndex === 0){
+                this.focusIndex = optionsLength - 1;
+            } else {
+                this.focusIndex = this.focusIndex - 1;
+            }
+            this.focusOption(this.focusIndex);
+        }, 0);
+    }
 
-  selectOption(){
-      setTimeout(()=>{ 
-          for(let i = 0; i < this.states.length; i++){ 
-              if(i == this.focusIndex){
-                  this.onListboxOptionSelected.emit(this.states[i].name); 
-              }
-          }
-      } , 0);
-  }
+    selectOption(){
+        setTimeout(()=>{ 
+            for(let i = 0; i < this.states.length; i++){ 
+               if(i == this.focusIndex){
+                   this.onListboxOptionSelected.emit(this.states[i].name); 
+               }
+            }
+        } , 0);
+    }
 
-  selectOptionWithClick(event: any){
-      this.focusIndex = event.target.id;
-      this.selectOption();
-  }
+    selectOptionWithClick(event: any){
+        this.focusIndex = event.target.id;
+        this.selectOption();
+    }
 
-  escapeListbox() {
-      this.onListboxEscPressed.emit(true);
-  }
+    escapeListbox() {
+        this.onListboxEscPressed.emit(true);
+    }
 
-  firstCharAlphaFocus(event: any) {
-      setTimeout(()=>{ 
-          let firstCharEntered = event.key;
-          for(let i = 0; i < this.states.length; i++){ 
-              if(firstCharEntered.toUpperCase() == this.states[i].name.charAt(0)){
-                  this.focusIndex = i;
-                  this.focusOption(this.focusIndex);
-                  break;
-              }
-          }
-    } , 0);
-  }
+    firstCharAlphaFocus(event: any) {
+        setTimeout(()=>{ 
+            let firstCharEntered = event.key;
+            for(let i = 0; i < this.states.length; i++){ 
+                if(firstCharEntered.toUpperCase() == this.states[i].name.charAt(0)){
+                    this.focusIndex = i;
+                    this.focusOption(this.focusIndex);
+                    break;
+                }
+            }
+        }, 0);
+    }
 
-  ngOnInit(): void { 
-      this.getStates();
-  }
+    ngOnInit(): void { 
+        this.getStates();
+    }
   
-  ngAfterViewInit() { 
-      this.focusOption(this.focusIndex); 
-  }
+    ngAfterViewInit() { 
+        this.focusOption(this.focusIndex); 
+    }
   
 }
 
