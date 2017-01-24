1fe38d945f4feeed93e188d6b406d719448bb4c6
Chris Lane <chris.lane359@gmail.com>
2017-01-23 19:18:40 -0800

    commented out attempt to press the tab key

--- repo/e2e/app.e2e-spec.ts
+++ repo/e2e/app.e2e-spec.ts
@@ -17,7 +17,17 @@ describe('Suite 1',  () => {
             return origFn.apply(browser.driver.controlFlow(), args);
         };
     });
-
+    
+    /*
+    //Begin spec to test the TAB key on the <body>
+    it('Press the tab key on the body', () => {
+        let body = element(by.name('body'));
+        body.sendKeys(protractor.Key.TAB);
+        browser.sleep(3000); 
+    });
+    //End spec to test the TAB key on the <body>
+    */
+    
     //Begin spec for <body> as active element
     it('Test for the tag name of the active element to be "BODY" after the page loads', () => {
          browser.executeScript('return document.activeElement.tagName').then( (tagName) => {
