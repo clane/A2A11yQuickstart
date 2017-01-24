30c123df6eb0b45b225ece174d8aad6b79895493
Chris Lane <chris.lane359@gmail.com>
2017-01-24 06:04:34 -0800

    CSS, content now has a white bg

--- repo/app/about.component.ts
+++ repo/app/about.component.ts
@@ -6,7 +6,20 @@ import { Title } from '@angular/platform-browser';
     template: `
         <h2>About</h2>
         <p>This Quickstart for Angular 2 is intended to give an example of Angular 2 basics well as 
-        examples of techniques and challenges pertinent to ARIA development.</p>
+        examples of techniques and challenges pertinent to accessible web development such as:</p>
+        <ol>
+            <li>Keyboard Accessibility</li>
+            <li>Focus Management</li>
+            <li>ARIA</li>
+        </ol>
+        
+        <p> It also demonstrates end-to-end testing with 
+        <a href="http://www.protractortest.org/#/">Protractor</a> and 
+        <a href="http://whatsock.com/training/matrices/visual-aria.htm">Visual ARIA</a>.</p>
+        <p>To see these tests in action
+        download or clone 
+        <a href="https://github.com/clane/A2A11yQuickstart">A2 A11y Quickstart</a>
+        </p>
 
         <h3>Resources</h3>
         <ul>
@@ -15,23 +28,18 @@ import { Title } from '@angular/platform-browser';
             </li>
             <li>
                 <a href="http://www.chrislane.info/angular-2-try-not-to-use-native-element-references/">
-                http://www.chrislane.info/angular-2-try-not-to-use-native-element-references/
+                Don't use native element references in Angular 2
                 </a>
             </li>
                
             <li>
                 <a href="http://www.chrislane.info/angular-2-beta-17-actually-ships-with-native-key-combination-support/">
-                Angular 2 Beta 17 actually ships with native key-combination support.
+                Angular 2 native key-combination support
                 </a>
             </li>
+            <li>
+                <a href="http://www.chrislane.info/protractor-debugging/">Protractor Debugging</a>
              <li>
-            </li>
-             <li>
-            </li>
-             <li>
-            </li>
-             <li>
-            </li>
         </ul>
     `,
 })
