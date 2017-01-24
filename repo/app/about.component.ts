476c39b06087e988273f226e3f43655c1532e582
Chris Lane <chris.lane359@gmail.com>
2017-01-23 21:04:58 -0800

    adding about content

--- repo/app/about.component.ts
+++ repo/app/about.component.ts
@@ -5,7 +5,34 @@ import { Title } from '@angular/platform-browser';
     selector: 'about',
     template: `
         <h2>About</h2>
-        <a href="https://github.com/clane/A2A11yQuickstart">Github Repo</a>
+        <p>This Quickstart for Angular 2 is intended to give an example of Angular 2 basics well as 
+        examples of techniques and challenges pertinent to ARIA development.</p>
+
+        <h3>Resources</h3>
+        <ul>
+            <li>
+                <a href="https://github.com/clane/A2A11yQuickstart">Github Repo for A2 A11y Quickstart</a>
+            </li>
+            <li>
+                <a href="http://www.chrislane.info/angular-2-try-not-to-use-native-element-references/">
+                http://www.chrislane.info/angular-2-try-not-to-use-native-element-references/
+                </a>
+            </li>
+               
+            <li>
+                <a href="http://www.chrislane.info/angular-2-beta-17-actually-ships-with-native-key-combination-support/">
+                Angular 2 Beta 17 actually ships with native key-combination support.
+                </a>
+            </li>
+             <li>
+            </li>
+             <li>
+            </li>
+             <li>
+            </li>
+             <li>
+            </li>
+        </ul>
     `,
 })
 export class AboutComponent implements AfterViewInit {
