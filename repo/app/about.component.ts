4f0237ac3ac7b9c9814d875b609c469cd4b01db9
Chris Lane <chris.lane359@gmail.com>
2017-01-30 18:31:45 -0800

    Removing resources section
    just going to use my blog

--- repo/app/about.component.ts
+++ repo/app/about.component.ts
@@ -21,26 +21,6 @@ import { Title } from '@angular/platform-browser';
         <a href="https://github.com/clane/A2A11yQuickstart">A2 A11y Quickstart</a>
         </p>
 
-        <h3>Resources</h3>
-        <ul>
-            <li>
-                <a href="https://github.com/clane/A2A11yQuickstart">Github Repo for A2 A11y Quickstart</a>
-            </li>
-            <li>
-                <a href="http://www.chrislane.info/angular-2-try-not-to-use-native-element-references/">
-                Don't use native element references in Angular 2
-                </a>
-            </li>
-               
-            <li>
-                <a href="http://www.chrislane.info/angular-2-beta-17-actually-ships-with-native-key-combination-support/">
-                Angular 2 native key-combination support
-                </a>
-            </li>
-            <li>
-                <a href="http://www.chrislane.info/protractor-debugging/">Protractor Debugging</a>
-             <li>
-        </ul>
     `,
 })
 export class AboutComponent implements AfterViewInit {
